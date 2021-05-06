e1ead368773ae7220e2549b6f3507e9ac0950ce8
FranOrtega <franc.ortegac@duocuc.cl>
2021-05-06 09:45:07 -0500

    Indicador dolar/Peso chileno

--- .js/ApiPrueba.js
+++ .js/ApiPrueba.js
@@ -1,13 +1,64 @@
-var map;
-  	 function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-		  center: {lat: -33.03233099768304, lng: -71.53397803629817},  
-          zoom: 13,
-        });
-        var marker = new google.maps.Marker({
-          position: {lat: -33.03233099768304, lng: -71.53397803629817},
-          map: map,
-	  title: 'Sede Duoc Uc, Viña del mar'
-        });
-      }
- 
+$(document).ready(function(){
+
+	var indicadores = {
+		dolar: 0,
+		euro: 0,
+		utm: 0,
+		uf: 0,
+	}
+
+	//var dolar, euro, utm, uf
+
+	$.ajax({
+		url: 'http://www.mindicador.cl/api',
+		method: 'GET'
+	}).done(function(data){
+		console.log(data); //esto venia con el ejemoplo
+		indicadores.dolar = data.dolar.valor,
+		indicadores.euro = data.euro.valor,
+		indicadores.utm = data.utm.valor,
+		indicadores.uf = data.uf.valor,
+
+		$(".uf__text").append("$" + indicadores.uf);
+		$(".dolar__text").append("usd$" + indicadores.dolar);
+		$(".utm__text").append(indicadores.utm);
+		$(".euro__text").append("$" + indicadores.euro);
+	});
+
+	
+
+	
+	$('.dolar__button').on('click', function(e) {
+		var input = parseInt( $('.dolar__input').val() );
+		$('.dolar__answer').text( input / indicadores.dolar );
+		$('.dolar__input').val('');
+	});
+
+	
+
+	$('.euro__button').on('click', function(e) {
+		var input = parseInt( $('.euro__input').val() );
+		$('.euro__answer').text( input / indicadores.euro );
+	});
+
+
+	$('.utm__button').on('click', function(e) {
+		var input = parseInt( $('.utm__input').val() );
+		$('.utm__answer').text( input / indicadores.utm );
+	});
+
+
+	$('.uf__button').on('click', function(e) {
+		var input = parseInt( $('.uf__input').val() );
+		$('.uf__answer').text( input / indicadores.uf  );
+
+	});
+
+	$('[class$="__reset"]').on('click', function(){
+		$(this).prev().prev().prev().val('');
+		$(this).prev().text('');
+	});
+	
+
+
+});
